3ee9ee3b111aa7bdfff497fd9c04fe4fe97be225
Claude Andrianarijaona <candrianarijaona@infopro-digital.com>
2018-04-08 11:01:11 +0200

    deleting unused dependencies

--- app/settings.php
+++ app/settings.php
@@ -4,21 +4,5 @@ return [
         // Slim Settings
         'determineRouteBeforeAppMiddleware' => false,
         'displayErrorDetails' => false,
-
-        // View settings
-        'view' => [
-            'template_path' => __DIR__ . '/templates',
-            'twig' => [
-                'cache' => __DIR__ . '/../cache/twig',
-                'debug' => true,
-                'auto_reload' => true,
-            ],
-        ],
-
-        // monolog settings
-        'logger' => [
-            'name' => 'app',
-            'path' => __DIR__ . '/../log/app.log',
-        ],
     ],
 ];
