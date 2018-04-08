03640b21d43ac00ab458dfe51b55951c56da5eb9
Claude Andrianarijaona <candrianarijaona@infopro-digital.com>
2018-04-08 11:00:44 +0200

    adding anonymous function

--- app/routes.php
+++ app/routes.php
@@ -6,3 +6,8 @@ use Example\Controller\HomeController;
 $app->map(['POST', 'GET'], '/', HomeController::class . ':indexAction')
     ->setName('homepage')
 ;
+
+$app->get('/hello/{name}', function ($request, $response, $args) {
+    $response->write("Hello, " . $args['name']);
+    return $response;
+});
